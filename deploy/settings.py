f3e01a3c04ee844eec064751a6b3dda5e51c6235
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2018-05-30 23:11:40 +0300

    Add Whitenoise for container static file serving

---
+++ deploy/settings.py
@@ -0,0 +1,11 @@
+
+from respa.settings import *
+
+# Get whitenoise for serving static files
+try:
+    place = MIDDLEWARE.index('django.middleware.security.SecurityMiddleware')
+except ValueError:
+    place = 0
+
+MIDDLEWARE.insert(place, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
