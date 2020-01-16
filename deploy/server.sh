56001e86fbbcd3ffbd9388b1aae2e895fa8c6b91
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2020-01-16 19:12:26 +0200

    Add logging, threading and worker setting to uwsgi

--- deploy/server.sh
+++ deploy/server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "NOTICE: Get static files for serving"
-./manage.py collectstatic --no-input
+python3 ./manage.py collectstatic --no-input
 
 echo "NOTICE: Start the uwsgi web server"
-exec uwsgi --http :8000 --wsgi-file deploy/wsgi.py --check-static /usr/src/app/www
+exec uwsgi --http :8000 --wsgi-file deploy/wsgi.py --check-static /usr/src/app/www --logto2 /logs/uwsgi.log -p 10 -T
